b938b8348b58af7e943a8dd6afff146cfd97f80e
peyo <peteryyoon@gmail.com>
2020-03-20 10:25:14 -0700

    added 1 dynos

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const POKEDEX = require("./pokedex.json");
 
 const app = express();
 
-console.log(process.env.API_TOKEN);
-
-app.use(morgan("dev"));
-app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
+const morganSetting = process.env.NODE_ENV === "product" ? "tiny" : "common"
+app.use(morgan(morganSetting));
+app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0"}));
 app.use(cors());
 
 const validTypes =
@@ -26,8 +25,6 @@ const validTypes =
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-
-  console.log("validate bearer token middleware");
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
@@ -65,7 +62,17 @@ app.get("/pokemon", function handleGetPokemon(req, res) {
   res.json(result);
 });
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
